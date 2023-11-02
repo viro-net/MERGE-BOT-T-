8d1b15927a41ed5448550fb6297320188bee9f57
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-11-03 00:31:59 +0530

    Update cb_handler.py

--- plugins/cb_handler.py
+++ plugins/cb_handler.py
@@ -140,7 +140,7 @@ async def callback_handler(c: Client, cb: CallbackQuery):
                 "Current filename: **[@JAsuranserials]_Merged.mkv**\n\nSend me new file name without extension: You have 1 minute"
             )
             res: Message = await c.listen(
-                (cb.message.chat.id,None,None), filters=Filters.text, timeout=150
+                (cb.message.chat.id,None,None), filters=filters.text, timeout=150
             )
             if res.text:
                 new_file_name = f"downloads/{str(cb.from_user.id)}/{res.text}.mkv"
