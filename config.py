2223916ee503c668d969751f875812e8ee04a247
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-11-08 21:00:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class Config(object):
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://leecher:leecher@cluster0.606mkpi.mongodb.net/?retryWrites=true&w=majority")
     LOGCHANNEL = os.environ.get("LOGCHANNEL", "-1002119311182")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "AQAaiBkAt9DsH8XO1z9_dxUkBgcLvnAKii6M9A5uFZi3coeUdFVTOPfNzMqjv6PGUgQa3hoE4uAkyEh7E531j1mGHxNBk8b9NSIV0KibnhJLVGsullkw5ge1H_qUoBCLzy3XHC1J_a9SfEu_5j_41cv82B-Ee1D-gF5luTdMe8W2-m77o8JDS8z1zUwS8zgYMyFy7ZSaoC7u49U8YCBARbaRhxThWwcJVtboFgv-BGacEiMYDtU-xHvgX4Zu1TkYLaz_9neSp4Tt__MYGIt8xttL5qYIYWJz42z_agyB0ktWzIujAU-LF7KTn5zVLq4xh-A3WSih4yitslDc2DvCty_GEAcRgwAAAABRsDH9AA")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "AQAaiBkAuw3lf8-ZdKhp0I4LsYh__v-nmBcApt5jiO_R1heFGAf-iJyKikc1xcwC1-mK5aT-vqT3he-aaOYpU1sHt9b1pjTByVZwlCCZkOl5XbWhc1kUOs1Tqg1hQlX35hZkNcu0UfBLnGsfUFy1V19C6KeQkq9210DTzmq0Ol8eOGtTgXT-3Tqw2fAMUpSnSU-c869_hmHlHbecSkHCeBy1KtZNrLXt_D1I9KLV-qf-Puh2FEWNA3NfT-0sdnRI46gOl4BHOQNT8wcUV1VQzmZVHobA3FV2cnztOLHipb986bP-0czB5B5GXOZ99ShnQisn4zFGNUXpOs4cl4-nGO-wbCdkIQAAAABRsDH9AA")
     IS_PREMIUM = True
     MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
